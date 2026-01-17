de482dc1bf36cfd4a5205afd58334efc42180ff6
Adnan Manik <adnanmanik0@gmail.com>
2026-01-17 22:43:32 +0500

    added package file

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { tekmetricWebhook } from './handlers.js';
 dotenv.config();
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
